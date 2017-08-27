8addc4f052d8b11cbea5bc0dceaabbcbac98b43d
Justin Michalicek <jmichalicek@gmail.com>
2017-08-27 23:50:19 +0000

    better github release and then build script and remove sudo in run_django.sh now that DO spaces are in use

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-#BUILD_NUM=`date +%Y%m%d_%H%M%S`
-docker build --rm -f Dockerfile -t bash-shell-net:$VERSION --build-arg REPO_REFERENCE=$VERSION .
+# TODO: Allow this stuff to be overridden on the command line or with environment variables
+BUILD_NUM=`date +%Y%m%d_%H%M%S`
+GIT_TAG="v$BUILD_NUM"
+DOCKER_TAG="$BUILD_NUM"
+
+TAG_JSON="{\"tag_name\": \"$GIT_TAG\", \"target_commitish\": \"master\", \"name\": \"$GIT_TAG\", \"body\": \"Release of version $BUILD_NUM.\", \"draft\": false, \"prerelease\": false}"
+
+curl -s -k -X POST -H "Content-Type: application/json" "https://api.github.com/repos/jmichalicek/bash-shell.net/releases?access_token=$GITHUB_ACCESS_TOKEN" -d "$TAG_JSON"
+docker build --rm -f Dockerfile -t registry.gitlab.com/jmichalicek/bash-shell.net:$DOCKER_TAG --build-arg REPO_REFERENCE=$GIT_TAG .
+docker push registry.gitlab.com/jmichalicek/bash-shell.net
--- run_django.sh
+++ run_django.sh
@@ -5,6 +5,6 @@
 cd $HOME/bash-shell.net/app/
 PYENV_DIR=$(pyenv root)
 # no need to sudo as soon as these go to digital ocean spaces
-sudo -E $PYENV_DIR/versions/blog/bin/python manage.py collectstatic --no-input
+$(pyenv root)/versions/blog/bin/python manage.py collectstatic --no-input
 $(pyenv root)/versions/blog/bin/python manage.py migrate --no-input
 $(pyenv root)/versions/blog/bin/gunicorn --workers 2 --bind 0.0.0.0:8000 --name django --max-requests=1000 bash_shell_net.wsgi:application
